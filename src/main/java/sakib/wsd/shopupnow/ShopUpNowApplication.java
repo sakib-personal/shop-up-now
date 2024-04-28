c115af58af3bb64a6a015d8789d2ac89517bbc4d
Sakib Hossain <sakib.hossain@bjitgroup.com>
2024-04-28 13:12:17 +0600

    Spring boot initial basic source code

---
+++ src/main/java/sakib/wsd/shopupnow/ShopUpNowApplication.java
@@ -0,0 +1,13 @@
+package sakib.wsd.shopupnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopUpNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopUpNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sakib/wsd/shopupnow/ShopUpNowApplicationTests.java
@@ -0,0 +1,13 @@
+package sakib.wsd.shopupnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUpNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
